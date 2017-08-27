081a5b7e5b9dd2809595bef40d6d90bfee2dcd64
sisisin <azsisisin@gmail.com>
2017-08-28 01:07:11 +0900

    feat: setup heroku

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const RedisOption = isProd ? {
-  port: process.env.REDIS_PORT,
-  host: process.env.REDIS_HOST,
-  pass: process.env.REDIS_PW,
+  url: process.env.REDIS_URL,
   ttl: 259200 // sec. 3day expire
 } : {
-    port: 6379,
-    host: 'localhost',
     ttl: 86400 // sec. 1day expire
   };
 
@@ -15,7 +11,7 @@ const callbackPath = '/auth/twitter/callback';
 const PassportOption = {
   consumerKey: process.env.SCHEDULE_NOTIFIER_TWITTER_CONSUMER_KEY,
   consumerSecret: process.env.SCHEDULE_NOTIFIER_TWITTER_CONSUMER_SECRET,
-  callbackURL: isProd ? `http://localhost:3000${callbackPath}` : `http://localhost:3000${callbackPath}`,
+  callbackURL: isProd ? `https://schedule-notifier.herokuapp.com${callbackPath}` : `http://localhost:3000${callbackPath}`,
   passReqToCallback: true  
 };
 
@@ -23,7 +19,7 @@ const googleCallbackPath = '/auth/google/callback';
 const GoogleOption = {
   clientID: process.env.SCHEDULE_NOTIFIER_GOOGLE_CLIENT_ID,
   clientSecret: process.env.SCHEDULE_NOTIFIER_GOOGLE_CLIENT_SECRET,
-  callbackURL: isProd ? `http://localhost:3000${googleCallbackPath}` : `http://localhost:3000${googleCallbackPath}`,
+  callbackURL: isProd ? `https://schedule-notifier.herokuapp.com${googleCallbackPath}` : `http://localhost:3000${googleCallbackPath}`,
   passReqToCallback: true
 };
 module.exports = { PassportOption, RedisOption, GoogleOption };
